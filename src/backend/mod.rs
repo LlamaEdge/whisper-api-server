45d11e2236a1a4b0d91e99ec58547fba55322c52
Xin Liu <sam@secondstate.io>
2024-10-16 20:17:08 +0900

    chore: improve `handle_llama_request` func
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,13 +1,46 @@
 pub(crate) mod whisper;
 
-use crate::error;
+use crate::{error, TaskType, TASK};
 use hyper::{Body, Request, Response};
 use whisper::{whisper_transcriptions_handler, whisper_translations_handler};
 
 pub(crate) async fn handle_llama_request(req: Request<Body>) -> Response<Body> {
+    // get task
+    let task = match TASK.get() {
+        Some(task) => task,
+        None => {
+            let err_msg = "The task is not set.";
+
+            // log
+            error!(target: "stdout", "{}", &err_msg);
+
+            return error::internal_server_error("The task is not set.");
+        }
+    };
+
     match req.uri().path() {
-        "/v1/audio/transcriptions" => whisper_transcriptions_handler(req).await,
-        "/v1/audio/translations" => whisper_translations_handler(req).await,
+        "/v1/audio/transcriptions" => match task {
+            TaskType::Full | TaskType::Transcriptions => whisper_transcriptions_handler(req).await,
+            _ => {
+                let err_msg = "The current API server only support transcription tasks. To support translation and/or transcription tasks, please restart the API server with `--task full` or `--task translate`.";
+
+                // log
+                error!(target: "stdout", "{}", &err_msg);
+
+                error::internal_server_error(err_msg)
+            }
+        },
+        "/v1/audio/translations" => match task {
+            TaskType::Full | TaskType::Translations => whisper_translations_handler(req).await,
+            _ => {
+                let err_msg = "The current API server only support translation tasks. To support transcription and/or translation tasks, please restart the API server with `--task full` or `--task transcribe`.";
+
+                // log
+                error!(target: "stdout", "{}", &err_msg);
+
+                error::internal_server_error(err_msg)
+            }
+        },
         _ => error::invalid_endpoint(req.uri().path()),
     }
 }
