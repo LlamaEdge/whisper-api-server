9b0ca2a822c3574868fd1b3f23f083afe78050b0
Xin Liu <sam@secondstate.io>
2024-12-31 11:50:09 +0800

    feat: support file uploading
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/backend/whisper.rs
+++ src/backend/whisper.rs
@@ -7,8 +7,8 @@ use hyper::{body::to_bytes, Body, Method, Request, Response};
 use multipart::server::{Multipart, ReadEntry, ReadEntryResult};
 use multipart_2021 as multipart;
 use std::{
-    fs,
-    io::{Cursor, Read},
+    fs::{self, File},
+    io::{Cursor, Read, Write},
     path::Path,
     time::SystemTime,
 };
@@ -1269,7 +1269,177 @@ pub(crate) async fn files_handler(req: Request<Body>) -> Response<Body> {
     // log
     info!(target: "stdout", "Handling the coming files request");
 
-    let res = if req.method() == Method::GET {
+    let res = if req.method() == Method::POST {
+        let boundary = "boundary=";
+
+        let boundary = req.headers().get("content-type").and_then(|ct| {
+            let ct = ct.to_str().ok()?;
+            let idx = ct.find(boundary)?;
+            Some(ct[idx + boundary.len()..].to_string())
+        });
+
+        let req_body = req.into_body();
+        let body_bytes = match to_bytes(req_body).await {
+            Ok(body_bytes) => body_bytes,
+            Err(e) => {
+                let err_msg = format!("Fail to read buffer from request body. {}", e);
+
+                // log
+                error!(target: "stdout", "{}", &err_msg);
+
+                return error::internal_server_error(err_msg);
+            }
+        };
+
+        let cursor = Cursor::new(body_bytes.to_vec());
+
+        let mut multipart = Multipart::with_body(cursor, boundary.unwrap());
+
+        let mut file_object: Option<FileObject> = None;
+        while let ReadEntryResult::Entry(mut field) = multipart.read_entry_mut() {
+            if &*field.headers.name == "file" {
+                let filename = match field.headers.filename {
+                    Some(filename) => filename,
+                    None => {
+                        let err_msg =
+                            "Failed to upload the target file. The filename is not provided.";
+
+                        // log
+                        error!(target: "stdout", "{}", &err_msg);
+
+                        return error::internal_server_error(err_msg);
+                    }
+                };
+
+                if !((filename).to_lowercase().ends_with(".txt")
+                    || (filename).to_lowercase().ends_with(".md")
+                    || (filename).to_lowercase().ends_with(".png")
+                    || (filename).to_lowercase().ends_with(".wav"))
+                {
+                    let err_msg = format!(
+                        "Failed to upload the target file. Only files with 'txt', 'md', 'png', 'wav' extensions are supported. The file to be uploaded is {}.",
+                        &filename
+                    );
+
+                    // log
+                    error!(target: "stdout", "{}", &err_msg);
+
+                    return error::internal_server_error(err_msg);
+                }
+
+                let mut buffer = Vec::new();
+                let size_in_bytes = match field.data.read_to_end(&mut buffer) {
+                    Ok(size_in_bytes) => size_in_bytes,
+                    Err(e) => {
+                        let err_msg = format!("Failed to read the target file. {}", e);
+
+                        // log
+                        error!(target: "stdout", "{}", &err_msg);
+
+                        return error::internal_server_error(err_msg);
+                    }
+                };
+
+                // create a unique file id
+                let id = format!("file_{}", uuid::Uuid::new_v4());
+
+                // save the file
+                let path = Path::new("archives");
+                if !path.exists() {
+                    fs::create_dir(path).unwrap();
+                }
+                let file_path = path.join(&id);
+                if !file_path.exists() {
+                    fs::create_dir(&file_path).unwrap();
+                }
+                let mut file = match File::create(file_path.join(&filename)) {
+                    Ok(file) => file,
+                    Err(e) => {
+                        let err_msg =
+                            format!("Failed to create archive document {}. {}", &filename, e);
+
+                        // log
+                        error!(target: "stdout", "{}", &err_msg);
+
+                        return error::internal_server_error(err_msg);
+                    }
+                };
+                file.write_all(&buffer[..]).unwrap();
+
+                // log
+                info!(target: "stdout", "file_id: {}, file_name: {}", &id, &filename);
+
+                let created_at = match SystemTime::now().duration_since(std::time::UNIX_EPOCH) {
+                    Ok(n) => n.as_secs(),
+                    Err(_) => {
+                        let err_msg = "Failed to get the current time.";
+
+                        // log
+                        error!(target: "stdout", "{}", &err_msg);
+
+                        return error::internal_server_error(err_msg);
+                    }
+                };
+
+                // create a file object
+                file_object = Some(FileObject {
+                    id,
+                    bytes: size_in_bytes as u64,
+                    created_at,
+                    filename,
+                    object: "file".to_string(),
+                    purpose: "assistants".to_string(),
+                });
+
+                break;
+            }
+        }
+
+        match file_object {
+            Some(fo) => {
+                // serialize chat completion object
+                let s = match serde_json::to_string(&fo) {
+                    Ok(s) => s,
+                    Err(e) => {
+                        let err_msg = format!("Failed to serialize file object. {}", e);
+
+                        // log
+                        error!(target: "stdout", "{}", &err_msg);
+
+                        return error::internal_server_error(err_msg);
+                    }
+                };
+
+                // return response
+                let result = Response::builder()
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Methods", "*")
+                    .header("Access-Control-Allow-Headers", "*")
+                    .header("Content-Type", "application/json")
+                    .body(Body::from(s));
+
+                match result {
+                    Ok(response) => response,
+                    Err(e) => {
+                        let err_msg = e.to_string();
+
+                        // log
+                        error!(target: "stdout", "{}", &err_msg);
+
+                        error::internal_server_error(err_msg)
+                    }
+                }
+            }
+            None => {
+                let err_msg = "Failed to upload the target file. Not found the target file.";
+
+                // log
+                error!(target: "stdout", "{}", &err_msg);
+
+                error::internal_server_error(err_msg)
+            }
+        }
+    } else if req.method() == Method::GET {
         let uri_path = req.uri().path().trim_end_matches('/').to_lowercase();
 
         // Split the path into segments
